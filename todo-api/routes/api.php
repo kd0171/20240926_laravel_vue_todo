79dc41a67663b4e6734da371d2e2ba8fbdd4b5a2
kd0171 <kido.0117.1895@gmail.com>
2024-09-22 19:26:39 +0200

    api.phpの変更

--- todo-api/routes/api.php
+++ todo-api/routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
+// ユーザー認証が必要なルート（必要であればそのまま残す）
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// ToDo APIのルート設定
+Route::get('/todos', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
+Route::put('/todos/{todo}', [TodoController::class, 'update']);
+Route::delete('/todos/{todo}', [TodoController::class, 'destroy']);
